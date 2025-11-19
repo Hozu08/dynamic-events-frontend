2c51abdc0d3f424372a6c776fbbd9253b6f6b4a5
Luis <alejocast.c08@gmail.com>
2025-11-19 02:11:08 -0500

    Configuraciones adicionales para gh pages

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react-swc'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: 'dynamic-events-frontend', 
+  base: 'dynamic-events-frontend', // ⚠️ Cambia esto por el nombre de TU repositorio
   server: {
     port: 5173
   }
